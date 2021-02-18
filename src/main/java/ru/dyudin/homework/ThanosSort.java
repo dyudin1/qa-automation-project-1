445256dd752a9e4936ff29dc7f4a5ac689ef5356
d.yudin1 <R5o9HbaG>
2021-02-18 22:25:46 +0300

    Add ThanosSort

---
+++ src/main/java/ru/dyudin/homework/ThanosSort.java
@@ -0,0 +1,71 @@
+package ru.dyudin.homework;
+
+import java.util.Arrays;
+
+public class ThanosSort {
+
+    static int[] sortByHalves(int[] notSortedArray) {
+        int[] sortedArray = Arrays.copyOf(notSortedArray, notSortedArray.length);
+
+        if (notSortedArray.length > 2) {
+
+            notSortedArray = sortArray(notSortedArray);
+
+            int cache = 0;
+            for (int j : notSortedArray) {
+                cache = cache + j;
+            }
+            double meanNum = (double) cache / notSortedArray.length;
+            int leftHalf = 0;
+            for (int l : notSortedArray) {
+                if (l < meanNum) {
+                    leftHalf++;
+                }
+            }
+
+            int[] cacheFirstHalf = Arrays.copyOfRange(notSortedArray, 0, leftHalf);
+            int[] cacheSecondHalf = Arrays.copyOfRange(notSortedArray, leftHalf, notSortedArray.length);
+
+
+            System.out.println("FirstHalf = " + Arrays.toString(cacheFirstHalf) + " SecondHalf = "
+                    + Arrays.toString(cacheSecondHalf) + "\n");
+
+            System.arraycopy(sortByHalves(cacheFirstHalf), 0, sortedArray, 0, cacheFirstHalf.length);
+            System.arraycopy(sortByHalves(cacheSecondHalf), 0, sortedArray,
+                    sortedArray.length - cacheSecondHalf.length, cacheSecondHalf.length);
+
+            return sortedArray;
+        }
+
+        return sortArray(notSortedArray);
+    }
+
+    static int[] sortArray(int[] notSortedArray) {
+
+        if (notSortedArray.length < 2) {
+            return notSortedArray;
+        }
+
+        int cache = 0;
+        for (int j : notSortedArray) {
+            cache = cache + j;
+        }
+        double meanNum = (double) cache / notSortedArray.length;
+
+        int[] sortedArray = Arrays.copyOf(notSortedArray, notSortedArray.length);
+        int x = 0;
+        int y = notSortedArray.length - 1;
+
+        for (int j : notSortedArray) {
+            if (j <= meanNum) {
+                sortedArray[x++] = j;
+            } else {
+                sortedArray[y--] = j;
+            }
+        }
+
+        System.out.println("Array sorted, result =" + Arrays.toString(sortedArray));
+
+        return sortedArray;
+    }
+}
--- src/main/java/ru/dyudin/homework/main.java
+++ src/main/java/ru/dyudin/homework/main.java
@@ -3,14 +3,21 @@ package ru.dyudin.homework;
 import java.util.Arrays;
 
 import static ru.dyudin.homework.CheckAndFixPhoneNum.fixNumber;
+import static ru.dyudin.homework.ThanosSort.*;
 
 public class main {
     public static void main(String[] args) {
 
         //Проверки исправления номера
-        System.out.println(Arrays.asList(fixNumber("+7 (999) 636 44 33")));
-        System.out.println(Arrays.asList(fixNumber("+7 999 636 44 313")));
-        System.out.println(Arrays.asList(fixNumber("89996364433")));
+//        System.out.println(Arrays.asList(fixNumber("+7 (999) 636 44 33")));
+//        System.out.println(Arrays.asList(fixNumber("+7 999 636 44 313")));
+//        System.out.println(Arrays.asList(fixNumber("89996364433")));
+
+        //Проверка сортировки
+//        int[] notSortedArray = {97, 25, 42, 90, 71, 16, 82, 39, 36, 74};
+//        int[] notSortedArray2 = {35, 11, 92, 13, 54, 72, 65, 91, 10, 5, 24, 64, 6};
+//        System.out.println(Arrays.toString(sortByHalves(notSortedArray)));
+//        System.out.println(Arrays.toString(sortByHalves(notSortedArray2)));
 
     }
 }
