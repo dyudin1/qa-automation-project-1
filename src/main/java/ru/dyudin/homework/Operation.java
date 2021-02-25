b59a2a2861411e010a2e487dc2a7c7af94b873b9
d.yudin1 <R5o9HbaG>
2021-02-25 12:21:03 +0300

    Refactor

--- src/main/java/ru/dyudin/homework/Operation.java
+++ src/main/java/ru/dyudin/homework/Operation.java
@@ -5,88 +5,92 @@ import java.util.Scanner;
 public class Operation {
 
     public static void main(String[] args) {
+
+        System.out.println("Type \"stop\" to exit");
+
         while (true) {
-            System.out.println("Start");
+            System.out.println("Enter expression to calculate");
+
             Scanner input = new Scanner(System.in);
             String expression = input.nextLine().strip();
-            if (expression.equals("stop")){
+
+            if (expression.equals("stop")) {
                 break;
             }
 
             String[] split = expression.split("[ ]+");
-            String operator = split[1];
 
+            if (split.length != 3) {
+                System.out.println("Invalid expression");
+                break;
+            }
+
+            String operator = split[1];
             Operation xyz = new Operation();
             xyz.num1 = Integer.parseInt((split[0]));
             xyz.num2 = Integer.parseInt(split[2]);
 
             switch (operator) {
                 case "+":
-                    new Plus().execute(xyz.num1, xyz.num2);
+                    System.out.println("Result is " + new Plus().execute(xyz.num1, xyz.num2));
                     break;
                 case "-":
-                    new Minus().execute(xyz.num1, xyz.num2);
+                    System.out.println("Result is " + new Minus().execute(xyz.num1, xyz.num2));
                     break;
                 case "*":
-                    new Multiply().execute(xyz.num1, xyz.num2);
+                    System.out.println("Result is " + new Multiply().execute(xyz.num1, xyz.num2));
                     break;
                 case "/":
-                    new Divide().execute(xyz.num1, xyz.num2);
+                    System.out.println("Result is " + new Divide().execute(xyz.num1, xyz.num2));
                     break;
                 default:
-                    System.out.println("Not supported");
-                    return;
+                    System.out.println(new Operation().execute(xyz.num1, xyz.num2));
+                    break;
             }
+
         }
     }
 
-    Operation(){
-    }
-
-    void execute(int num1, int num2) {
+    String execute(int num1, int num2) {
+        return "Not supported";
     }
 
     int num1;
     int num2;
 
-
     static class Plus extends Operation {
-        Plus() {
-        }
 
         @Override
-        void execute(int num1, int num2) {
-            System.out.println("Result is " + (num1 + num2));
+        String execute(int num1, int num2) {
+            return String.valueOf(num1 + num2);
         }
+
     }
 
     static class Minus extends Operation {
-        Minus() {
-        }
 
         @Override
-        void execute(int num1, int num2) {
-            System.out.println("Result is " + (num1 - num2));
+        String execute(int num1, int num2) {
+            return String.valueOf(num1 - num2);
         }
+
     }
 
     static class Divide extends Operation {
-        Divide() {
-        }
 
         @Override
-        void execute(int num1, int num2) {
-            System.out.println("Result is " + ((double) num1 / num2));
+        String execute(int num1, int num2) {
+            return String.valueOf((double) num1 / num2);
         }
+
     }
 
     static class Multiply extends Operation {
-        Multiply() {
-        }
 
         @Override
-        void execute(int num1, int num2) {
-            System.out.println("Result is " + (num1 * num2));
+        String execute(int num1, int num2) {
+            return String.valueOf(num1 * num2);
         }
+
     }
 }
